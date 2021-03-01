10e60b7b6559ec4f65092f03fdd5cd9376ab8e87
EltrutLikes <10eltrut1@gmail.com>
2021-02-28 21:25:11 -0500

    all wandering trades

--- src/main/java/com/evoslab/mobfulbricks/core/other/MBTrades.java
+++ src/main/java/com/evoslab/mobfulbricks/core/other/MBTrades.java
@@ -18,7 +18,21 @@ public class MBTrades {
 	@SubscribeEvent
 	public static void onWandererTradesEvent(final WandererTradesEvent event) {
 		TradeUtil.addWandererTrades(event,
-				new AbnormalsTrade(EMERALDS, MBBlocks.BEE_BRICKS.get().asItem(), BLOCKS, 16, 5));
+				new AbnormalsTrade(EMERALDS, MBBlocks.BEE_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.BLAZE_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.CARMEN_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.HOGLIN_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.PANDA_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.POLAR_BEAR_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.SPUD_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.STEVE_BRICKS.get().asItem(), BLOCKS, 16, 5),
+				new AbnormalsTrade(EMERALDS, MBBlocks.STRIDER_BRICKS.get().asItem(), BLOCKS, 16, 5));
+		
+		TradeUtil.addCompatWandererTrades(event, MBMods.ENDERGETIC_EXPANSION,
+				new AbnormalsTrade(EMERALDS, MBBlocks.BOOFLO_BRICKS.get().asItem(), BLOCKS, 16, 5));
+		
+		TradeUtil.addCompatWandererTrades(event, MBMods.ENVIRONMENTAL,
+				new AbnormalsTrade(EMERALDS, MBBlocks.SLABFISH_BRICKS.get().asItem(), BLOCKS, 16, 5));
 	}
 	
 }
